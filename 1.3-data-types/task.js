d31777aee6ee0569a3874a8a1627c125afd8d3bb
KuzminaYuliya <kuzmina_h@mail.ru>
2021-02-27 20:33:13 +0500

    calculator is corrected

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -3,27 +3,35 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
    let today = new Date();
    let percentNumber= +percent/100;
-        if (isNaN(percentNumber)) {
+        if (percentNumber <= 0 || isNaN(percentNumber)) {	
         return `Параметр \"Процентная ставка \" содержит неправильное значение ${percent}`
     }
    
    let contributionNumber = +contribution;
-        if (isNaN(contributionNumber)) {
+       	if (contributionNumber < 0 || isNaN(contributionNumber)) {
         return `Параметр \"Сумма первоначального взноса \" содержит неправильное значение ${contribution}`; 
     } 
    
    let amountNumber = +amount;
-        if (isNaN(amountNumber)) {
+        if (amountNumber < 0 || isNaN(amountNumber)) {	
         return `Параметр \"Сумма кредита \" содержит неправильное значение ${amount}`; 
     }
 
+    if (amountNumber < contributionNumber) {
+    	return `Параметр \"Сумма кредита \" содержит неправильное значение ${amount}`; 
+    }
+
    let dateFrom = new Date(date);
+
+   if (dateFrom <= today) {
+   		return `Параметр \"Срок ипотеки \" содержит неправильное значение ${date}`; 
+   }
 
    let kredit = amountNumber - contributionNumber;
    let month = dateFrom.getMonth() - today.getMonth() + (12*(dateFrom.getFullYear() - today.getFullYear()));
    let payment = kredit * (percentNumber * (1 / 12) + ((percentNumber * (1 / 12)) / ((Math.pow((1 + percentNumber * (1 / 12)),month)-1))));
    let count = payment * month;
-   console.log(count.toFixed(2));
+   //console.log(count.toFixed(2));
    return +count.toFixed(2);
 
 }   
