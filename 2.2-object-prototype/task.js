34897b7971aa1de7a243624347d43f045cc4a804
KuzminaYuliya <kuzmina_h@mail.ru>
2021-03-05 17:56:07 +0500

    first commit Task2.2-object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,43 @@
+"use strict";
 //String.prototype.isPalindrome - для задачи №1
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = function()  {
+	let flag = true;
+	let phrase = this.toLowerCase().replace(/\s/g, "");
+	for (let i = 0; i < (phrase.length / 2); i++) {
+		if (phrase[i] != phrase[phrase.length - i - 1]) {
+		flag = false;}
+	   }
+	return flag;
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+console.log ("А роза упала на лапу Азора".isPalindrome());	
+
+//Задача2
+function getAverageMark(marks) {
+    let quantity = marks.length;
+    let count = 0;
+    let average;
+    
+    if (quantity === 0) {
+        average = 0;
+        return average;
+    }
+   
+    for (let mark of marks) {
+        count = count + mark;
+    }
+    average = count / quantity;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;   
 }
+
+//Задача3
+function checkBirthday(birthday) {
+    let now = new Date().getTime();
+    let birthdayDate = new Date(birthday).getTime();
+    let diff = now - birthdayDate;
+    let age = diff / (1000 * 60 * 60 * 24 * 365);
+    return (age >= 18);
+}
+
